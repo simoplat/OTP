121f89459af91800b7218716e04a0dfb7e77754f
simoplat <150624084+simoplat@users.noreply.github.com>
2025-11-28 16:43:46 +0100

    Add OTP generation script with SHA256 hashing
    Introduces OTP.py, which generates a hash from a key and current timestamp, then computes a numeric value from the hash string. Includes a helper function to calculate the numeric value of the hash's characters.

---
+++ OTP.py
@@ -0,0 +1,45 @@
+import hashlib
+import datetime as TIME
+
+
+def calcola_valore_numerico(stringa_input: str) -> int:
+    risultato_stringa = ""
+    iterazione = 0
+    
+    for carattere in stringa_input:
+        # La funzione ord() restituisce il valore Unicode del carattere (un INT)
+        valore_numerico = ord(carattere)
+        
+        # 1. CONCATENAZIONE: Convertiamo il numero a stringa e lo concateniamo
+        risultato_stringa += str(valore_numerico)
+        
+        iterazione += 1
+        
+        # 2. Limitiamo l'operazione ai primi 3 caratteri (come richiesto)
+        if iterazione == 10:
+            break 
+
+    return int(risultato_stringa)
+
+
+
+orario_corrente = TIME.datetime.now().strftime("%Y-%m-%d %H:%M")
+chiave = "Sicurezza"
+stringa_da_hashare = chiave + orario_corrente
+stringa_da_hashare_pulita = stringa_da_hashare.replace(" ", "")
+print(f"Stringa da hashare: {stringa_da_hashare_pulita}")
+
+# I dati devono essere codificati in byte (.encode())
+oggetto_hash = hashlib.sha256(stringa_da_hashare_pulita.encode())
+
+# Ottenere l'hash in formato esadecimale
+esadecimale = oggetto_hash.hexdigest()
+
+
+
+print(f"Hash SHA256: {esadecimale}")
+print(f"Valore numerico totale dei caratteri della stringa: {calcola_valore_numerico(esadecimale)}")
+
+
+
+
